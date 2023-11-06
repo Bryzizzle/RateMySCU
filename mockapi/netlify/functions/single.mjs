b78c69c7a0dda0c982aeeb7966cadb523cb65705
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-05 18:01:33 -0800

    Mock: Bugfix

--- mockapi/netlify/functions/single.mjs
+++ mockapi/netlify/functions/single.mjs
@@ -3,7 +3,10 @@ import data from './data.json'
 export default async (req, context) => {
     const { path1 } = context.params;
 
-    return new Response(data["data"][path1]);
+    const resp = data["data"][path1];
+    console.log(resp);
+
+    return Response.json(resp);
 };
 
 export const config = {
