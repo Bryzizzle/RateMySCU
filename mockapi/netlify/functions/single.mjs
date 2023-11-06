089c4b98f0c61b061a54e88cd6461834558548d0
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-05 17:36:18 -0800

    Mock: Testing out data format to store responses

--- mockapi/netlify/functions/single.mjs
+++ mockapi/netlify/functions/single.mjs
@@ -1,9 +1,9 @@
-// const data = {
-//
-// }
+import data from './data.json' assert { type: 'json' }
 
 export default async (req, context) => {
   const { path1 } = context.params;
+
+  console.log(data)
 
   return new Response(`You're visiting ${path1}!`);
 };
