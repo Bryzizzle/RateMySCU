ca07fb23e4c224f3de5d19e67d36f7f1b35a46de
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-05 18:40:30 -0800

    Mock: Bugfix in response handling of double and triple depth paths

--- mockapi/netlify/functions/triple.mjs
+++ mockapi/netlify/functions/triple.mjs
@@ -2,14 +2,10 @@ import data from './data.json'
 
 export default async (req, context) => {
     const { path1, path2, path3 } = context.params;
-    let resp;
 
-    // Retrieve the associated response, if it fails then set it to undefined
-    try {
-        resp = data.data[path1][path2][path3];
-    } catch (e) {
-        resp = undefined;
-    }
+    // Retrieve the associated response
+    const path = `${path1}/${path2}/${path3}`
+    let resp = data.data[path];
 
     // If the request is not defined in the data.json file, then return a 404 response
     if (resp === undefined) {
