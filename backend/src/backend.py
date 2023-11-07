82d3017d96c5a4974b948067200ce2a78d1e4a3d
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-07 14:06:51 -0800

    Backend: Handled invalid endpoints, and non API calls

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api/v0")
 async def root():
     return {"status": "OK", "version": "dev"}
 
+
+@app.get("/api/{api_path:path}")
+async def invalid_endpoint():
+    raise HTTPException(status_code=404, detail="Invalid API Endpoint")
+
+
+@app.get("{full_path:path}")
+async def not_api_call():
+    return RedirectResponse("https://ratemyscu.scu.edu")
