67387be4908aa64feb0804a8b56c42e6000aabb7
Bryan <57810197+Bryzizzle@users.noreply.github.com>
2023-11-07 21:34:53 -0800

    Frontend: Added redirect for the root path

--- mockapi/netlify/functions/root.mjs
+++ mockapi/netlify/functions/root.mjs
@@ -1,9 +1,9 @@
 import data from './data.json'
 
 export default async (req, context) => {
-    return Response.redirect("https://ratemyscu.bryan.cf", 307)
+    return Response.redirect("https://ratemyscu.bryan.cf", 307);
 };
 
 export const config = {
-    path: "/"
+    path: "/:path"
 };
